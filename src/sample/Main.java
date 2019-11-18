51aba8187eda799d0347505d37cb9befca9e3dad
Bartosz Przybyl <bartolomeo369@gmail.com>
2019-11-18 21:12:09 +0100

    Add first window implementation

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Image icon = new Image("resources/images/icon.jpg");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("Accounting Application");
+        primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.show();
     }
 
--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,4 +1,31 @@
 package sample.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    public Button loginButton;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private void closeButtonAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void goToRegistration(ActionEvent actionEvent) {
+        System.out.println("Idz do rejestracji!");
+    }
+    @FXML
+    public void goToLogin(ActionEvent actionEvent) {
+        System.out.println("Idż do logowania!");
+    }
 }
